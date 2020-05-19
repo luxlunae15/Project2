e7e1c742da81b0b8111423008eb1bf03f34b9cb7
YeChanSong <54590290+YeChanSong@users.noreply.github.com>
2020-05-19 19:56:15 +0900

    파일 시간 가져오기 함수 구현
    구현 완료했습니다.

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -39,22 +39,34 @@ void filestat2(){
 
 //파일 1의 시간 정보를 가져오는 함수 작성
 void filetime1(){
-
+    time1 = localtime(&stat1.st_mtime);
 }
 
 //파일 2의 시간 정보를 가져오는 함수 작성
 void filetime2(){
-
+    time2 = localtime(&stat2.st_mtime);
 }
 
 //두 개의 파일 크기를 비교하는 함수 작성
 void sizecmp(){
-    
+	printf("size compare\n");
+	if(stat1.st_size > stat2.st_size)
+		printf("text1 is bigger\n\n");
+	else if (stat1.st_size < stat2.st_size)
+		printf("text2 is bigger\n\n");
+	else
+		printf("sizes are equal\n\n");
 }
 
 //두 개의 파일 블락 수를 비교하는 함수 작성
 void blockcmp(){
-    
+	printf("block compare\n");
+	if(stat1.st_blocks > stat2.st_blocks)
+		printf("text1 is bigger\n\n");
+	else if (stat1.st_blocks < stat2.st_blocks)
+		printf("text2 is bigger\n\n");
+	else
+		printf("sizes are equal\n\n");
 }
 
 //두 개의 파일 수정 날짜를 비교하는 함수 작성
